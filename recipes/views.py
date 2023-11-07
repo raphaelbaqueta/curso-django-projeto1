50e2b349777137da54951ff5c48b22fc3d27250c
Raphael Baqueta <raphaelbbarbosa@gmail.com>
2023-11-07 14:17:40 -0300

    Recipe detail view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, "recipes/pages/home.html", context={
             "name": "Raphael Baqueta",
         })
+
+
+def recipe(request, id):
+    return render(request, "recipes/pages/recipe-view.html", context={
+            "name": "Raphael Baqueta",
+        })
