c64b7e6ad813400000fcb8e79e856c6e89a4a84a
Raphael Baqueta <raphaelbbarbosa@gmail.com>
2023-11-27 15:57:59 -0300

    Adjust in category

--- recipes/models.py
+++ recipes/models.py
@@ -1,14 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
-
-
 class Category(models.Model):
     name = models.CharField(max_length=65)
-
     def __str__(self):
         return self.name
-
-
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -23,12 +18,14 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
-
     def __str__(self):
         return self.title
+    
