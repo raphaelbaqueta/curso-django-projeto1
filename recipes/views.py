c4849ae35aaf6488e76df90de09d262a595f1001
Raphael Baqueta <raphaelbbarbosa@gmail.com>
2024-02-09 14:04:10 -0300

    Changes until class 107

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -37,4 +38,12 @@ def recipe(request, id):
 
 
 def search(request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+    })
