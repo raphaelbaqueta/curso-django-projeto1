5b96d38626f1b659743ba1127cf1605c710162e8
Raphael Baqueta <raphaelbbarbosa@gmail.com>
2024-02-26 07:54:49 -0300

    Final version

--- project/settings/databases.py
+++ project/settings/databases.py
@@ -5,11 +5,7 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
